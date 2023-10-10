7041a0b908f8acf037c8122494de8864aad96b36
PoisonB3RRY <nickjianjian@outlook.com>
2023-10-10 18:14:27 +0800

    QT UI Design

---
+++ APP/src/app.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt6.QtWidgets import *
+from UI.src.Ui_demo1_helloworld import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    print(MainWindow.__mro__)
+
+    mainWindow = MainWindow()
+    mainWindow.show()
+
+    sys.exit(app.exec())
