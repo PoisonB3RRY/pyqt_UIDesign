3af8693b61344a718b21c9bbd650d03131478de2
PoisonB3RRY <nickjianjian@outlook.com>
2023-10-23 17:57:59 +0800

    change HeaderView

---
+++ UI/src/TableHeaderWithCheckBox.py
@@ -0,0 +1,8 @@
+import sys
+
+from PyQt6.QtWidgets import QWidget,QVBoxLayout,QTableWidget,QCheckBox,QHeaderView,QStyle,QStyleOptionButton,QTableWidgetItem
+from PyQt6.QtCore import Qt,pyqtSignal,QRect
+
+class CheckBoxHeader(QHeaderView):
+
+    ...
