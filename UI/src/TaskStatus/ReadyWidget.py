d4ee64ca0e08d50fd2a70417e75bf4fe3837d18f
PoisonB3RRY <nickjianjian@outlook.com>
2023-11-14 16:25:07 +0800

    Add new Status Widget

--- UI/src/TaskStatus/ReadyWidget.py
+++ UI/src/TaskStatus/ReadyWidget.py
@@ -9,7 +9,7 @@ from UI.src.const_res_path import ResPath
 
 #状态为成功的Widget，尺寸等属性从父对象继承
 #当前Widget中放置一个QSvgWidget用以显示Svg图形
-class SuccessWidget(QWidget):
+class ReadyWidget(QWidget):
 
     def __init__(self,parent=None):
         super().__init__()
@@ -19,7 +19,7 @@ class SuccessWidget(QWidget):
     def setupUI(self,parent):
         self.setObjectName('Success')
 
-        self.item = QGraphicsSvgItem(ResPath.SUCCESS_ICON_PATH)
+        self.item = QGraphicsSvgItem(ResPath.READY_ICON_PATH)
         self.item.setScale(0.15)
         self.scene = QGraphicsScene(20,32,50,50)
         self.scene.addItem(self.item)
@@ -40,6 +40,6 @@ class SuccessWidget(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    ex = SuccessWidget()
+    ex = ReadyWidget()
 
     sys.exit(app.exec())
